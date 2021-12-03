8b771616e65d106cac52ed2e2e0fc8e933253fd7
Eric G <peaceandhealth@protonmail.com>
2021-12-02 16:14:09 -0800

    form appears when add book button is clicked

--- logic.js
+++ logic.js
@@ -15,6 +15,28 @@ const fightClub = new Book("Fight Club", "Chuck", "180", "been read");
 console.log(fightClub.info());
 
 function addBookToLibrary () {
-    
+
 }
 
+function displayForm () {
+    document.getElementById('form1').style.display = 'block';
+}
+
+function hideForm () {
+    document.getElementById('form1').style.display = 'none';
+}
+
+
+const addBook = document.querySelector("#addbook");
+addBook.addEventListener('click', displayForm);
+
+const submitButton = document.querySelector("#submitbutton");
+submitButton.addEventListener('click', hideForm);
+
+// function displayForm () {
+//     shownForm = document.querySelector("#addBook");
+//     shownForm.style.display = "block";
+// }
+
+// const formButton = document.querySelector("#formSubmit");
+// formButton.addEventListener('click', displayForm());
